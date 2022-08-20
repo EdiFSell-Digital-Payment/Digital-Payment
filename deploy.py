5312c80d57441f3f860f64a63272204de2aaebc5
Edward Sellu <sellued@yahoo.com>
2022-08-20 15:35:31 +0000

    version2

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 #Deploying a python web application
-echo "Hello World"
-= shell scripts
-python('Hello World')
+#echo "Hello World"
+#= shell scripts
+#python('Hello World')
 #= python script
+print('Hello World') 
